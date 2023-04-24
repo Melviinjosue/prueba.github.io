3c3d26574686ae4bc1972e5b7510fecfc7a51f31
Melviinjosue <129076641+Melviinjosue@users.noreply.github.com>
2023-04-24 09:26:10 -0500

    main

--- operador.js
+++ operador.js
@@ -2,3 +2,4 @@
 var celsius1 = promp("Introduzca la temperatura en grados celsius: ");
 celsius1 = parseFloat(celsius1);
 var Farenheit1 = 9/5*celsius1+32;
+document.write("De celsius a Farenheit es:", Farenheit1);
