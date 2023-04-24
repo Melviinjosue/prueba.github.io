1582891abac4d7bda27d9c2c61dc5a4dd1e9d823
Melviinjosue <129076641+Melviinjosue@users.noreply.github.com>
2023-04-24 09:11:06 -0500

    main

--- operador.js
+++ operador.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-    <title>Conversion de grados Celsius a Fahrenheit</title>
-</head>
-<body>
-<script>
-   var grados = Fahrenheit= 9/5 Celsius +32;
-document.write =(grados);
-</script>
-</body>
-</html>
+  var grados = Fahrenheit= 9/5;
+  var celsius = celsius+32;
+document.write =(grados, celsius);
